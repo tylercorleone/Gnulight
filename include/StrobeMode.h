08bb3ebf4a7462c760d8dab06df418460bbd3998
Paolo Lupo <paolo.lupo.1985@gmail.com>
2018-12-17 12:38:45 +0100

    using SignalGenerator in StrobeMode

--- include/StrobeMode.h
+++ include/StrobeMode.h
@@ -7,11 +7,7 @@
 
 class Gnulight;
 
-#define STROBE_TYPES_COUNT 5
-
-enum StrobeTypes {
-	SINUSOIDAL_WAVE, TRIANGULAR_WAVE, ON_OFF_STROBE, BEACON_STROBE, DISCO_STROBE
-};
+#define SIGNAL_TYPES_COUNT 3
 
 class StrobeMode: public State<Gnulight, ButtonEvent> {
 public:
@@ -20,16 +16,12 @@ protected:
 	bool onEnterState(const ButtonEvent &event) override;
 	void onExitState() override;
 	bool handleEvent(const ButtonEvent &event) override;
-	static uint32_t onUpdate(StrobeMode *_this);
-	static float sinWave(uint32_t millis, uint32_t periodMs);
-	static float triangularWave(uint32_t millis, uint32_t periodMs);
-	Procedure &toggleLightStatusTask = ProcedureBuilder::begin(
-			StrobeMode::onUpdate, this).thenRepeat();
-	const StrobeTypes strobes[STROBE_TYPES_COUNT] = { SINUSOIDAL_WAVE,
-			TRIANGULAR_WAVE, ON_OFF_STROBE, BEACON_STROBE, DISCO_STROBE };
+	const SignalType signalTypes[SIGNAL_TYPES_COUNT] = {
+			SignalType::SINUSOIDAL_WAVE, SignalType::TRIANGULAR_WAVE,
+			SignalType::SQUARE_WAVE };
+	SignalGenerator *signalGenerator;
+	float levelMax = 0.0f;
 	uint8_t strobeIndex = 0;
-	uint32_t t_0 = -1;
-	float waveMaxLevel = 0.0;
 	uint16_t periodMultiplierX1000 = 1000UL;
 };
 
--- src/StrobeMode_cpp.h
+++ src/StrobeMode_cpp.h
@@ -2,51 +2,40 @@
 
 inline StrobeMode::StrobeMode(Gnulight &gnulight) :
 		State(gnulight, "StrobMod") {
+
+	signalGenerator = new SignalGenerator(gnulight.getTaskManager(),
+			gnulight.lightnessDimmer, STROBE_LEVEL_REFRESH_INTERVAL_MS);
 }
+
+#define THE_PERIOD ((uint32_t) STROBE_PERIODICAL_SEQUENCE_PERIOD_MS * periodMultiplierX1000 / 1000)
 
 inline bool StrobeMode::onEnterState(const ButtonEvent &event) {
 	logger.trace("index %d", strobeIndex);
 
-	t_0 = MILLIS_PROVIDER();
+	levelMax = Device().lightnessDimmer.setMainLevel(MainLightLevel::MED);
 
-	waveMaxLevel = Device().lightnessDimmer.setMainLevel(MainLightLevel::MED);
+	signalGenerator->start(signalTypes[strobeIndex], LEVEL_LOW_1, levelMax,
+			THE_PERIOD);
 
-	if (strobes[strobeIndex] == SINUSOIDAL_WAVE
-			|| strobes[strobeIndex] == TRIANGULAR_WAVE) {
-		Device().lightnessDimmer.setLevel(0.0f);
-		Device().lightnessDimmer.setState(OnOffState::ON);
-	}
-
-	Device().getTaskManager().StartTask(&toggleLightStatusTask);
+	Device().lightnessDimmer.setLevel(0.0f);
+	Device().lightnessDimmer.setState(OnOffState::ON);
 	return true;
 }
 
 inline void StrobeMode::onExitState() {
-	Device().getTaskManager().StopTask(&toggleLightStatusTask);
+	signalGenerator->stop();
 }
 
 inline bool StrobeMode::handleEvent(const ButtonEvent &event) {
-	t_0 = MILLIS_PROVIDER();
-
 	if (event.getClicksCount() > 0) {
 
 		switch (event.getClicksCount()) {
 		case 1:
 			Device().enterState(Device().offMode);
-			break;
+			return true;
 		case 2:
-			strobeIndex = (strobeIndex + 1) % STROBE_TYPES_COUNT;
+			strobeIndex = (strobeIndex + 1) % SIGNAL_TYPES_COUNT;
 			logger.debug("index %d", strobeIndex);
-
-			if (strobes[strobeIndex] == SINUSOIDAL_WAVE
-					|| strobes[strobeIndex] == TRIANGULAR_WAVE) {
-
-				MainLightLevel currentMainLevel =
-						Device().lightnessDimmer.getCurrentMainLevel();
-				waveMaxLevel = Device().lightnessDimmer.setMainLevel(
-						currentMainLevel);
-				Device().lightnessDimmer.setState(OnOffState::ON);
-			}
 			break;
 		case 3:
 			if (periodMultiplierX1000 <= 32000) {
@@ -63,92 +52,14 @@ inline bool StrobeMode::handleEvent(const ButtonEvent &event) {
 		}
 
 	} else if (event.getHoldStepsCount() > 0) {
-		// TODO: should we shut the light off before?
-		waveMaxLevel = Device().lightnessDimmer.setNextMainLevel();
+		levelMax = Device().lightnessDimmer.setNextMainLevel();
 	} else {
 		return false;
 	}
 
-	toggleLightStatusTask.setTimeInterval(0);
+	signalGenerator->start(signalTypes[strobeIndex], LEVEL_LOW_1, levelMax,
+			THE_PERIOD);
 	return true;
 }
 
-#define THE_PERIOD ((uint32_t) STROBE_PERIODICAL_SEQUENCE_PERIOD_MS * _this->periodMultiplierX1000 / 1000)
-#define CURRENT_STROBE (_this->strobes[_this->strobeIndex])
-
-inline uint32_t StrobeMode::onUpdate(StrobeMode *_this) {
-	uint32_t nextIntervalMs;
-	float nextLevel;
-
-	switch (CURRENT_STROBE) {
-
-	case ON_OFF_STROBE:
-		nextIntervalMs = (uint32_t) STROBE_ON_OFF_INIT_PERIOD_MS / 2
-				* _this->periodMultiplierX1000 / 1000;
-		break;
-	case BEACON_STROBE:
-		if (_this->Device().lightnessDimmer.getState() == OnOffState::OFF) {
-			nextIntervalMs = STROBE_BEACON_PERIOD_MS * STROBE_BEACON_DUTY_CYCLE;
-		} else {
-			nextIntervalMs = STROBE_BEACON_PERIOD_MS
-					* (1.0f - STROBE_BEACON_DUTY_CYCLE);
-		}
-		break;
-	case DISCO_STROBE:
-		if (_this->Device().lightnessDimmer.getState() == OnOffState::OFF) {
-			nextIntervalMs = STROBE_DISCO_PERIOD_MS * STROBE_DISCO_DUTY_CYCLE;
-		} else {
-			nextIntervalMs = STROBE_DISCO_PERIOD_MS
-					* (1.0f - STROBE_DISCO_DUTY_CYCLE);
-		}
-		break;
-	case SINUSOIDAL_WAVE:
-		nextIntervalMs = STROBE_LEVEL_REFRESH_INTERVAL_MS;
-		nextLevel = LEVEL_LOW_1
-				+ (_this->waveMaxLevel - LEVEL_LOW_1)
-						* (sinWave(MILLIS_PROVIDER() - _this->t_0, THE_PERIOD));
-		break;
-	case TRIANGULAR_WAVE:
-		nextIntervalMs = STROBE_LEVEL_REFRESH_INTERVAL_MS;
-		nextLevel = LEVEL_LOW_1
-				+ (_this->waveMaxLevel - LEVEL_LOW_1)
-						* triangularWave(MILLIS_PROVIDER() - _this->t_0,
-						THE_PERIOD);
-		break;
-	default:
-		return -1;
-	}
-
-	if (CURRENT_STROBE == SINUSOIDAL_WAVE
-			|| CURRENT_STROBE == TRIANGULAR_WAVE) {
-
-		/*
-		 * it is a wave sequence
-		 */
-		_this->Device().lightnessDimmer.setLevel(nextLevel);
-	} else {
-
-		/*
-		 * it is an ON/OFF strobe
-		 */
-		_this->Device().lightnessDimmer.toggleState();
-	}
-
-	return MsToTaskTime(nextIntervalMs);
-}
-
 #undef THE_PERIOD
-#undef CURRENT_STROBE
-
-inline float StrobeMode::triangularWave(uint32_t millis, uint32_t periodMs) {
-	millis %= periodMs;
-	if (millis < periodMs / 2) {
-		return static_cast<float>(millis) / (periodMs / 2);
-	} else {
-		return static_cast<float>(-(millis - periodMs)) / (periodMs / 2);
-	}
-}
-
-inline float StrobeMode::sinWave(uint32_t millis, uint32_t periodMs) {
-	return (-_cos(millis * (_TWO_PI / periodMs)) + 1.0f) / 2.0f;
-}
